c36706483ca3aed49375ead4fdf3ea4de3f4dd05
Ciro Spaciari MacBook <ciro@anthropic.com>
2026-05-24 16:15:47 -0700

    postMessage: node-compatible message for an invalid options.transfer
    A non-iterable (or bad-iterator) options.transfer now throws
    ERR_INVALID_ARG_TYPE 'Optional options.transfer argument must be an iterable'
    like node, instead of a generic 'Value is not a sequence' sequence error.

--- src/jsc/bindings/webcore/JSStructuredSerializeOptions.cpp
+++ src/jsc/bindings/webcore/JSStructuredSerializeOptions.cpp
@@ -22,6 +22,8 @@
 #include "JSStructuredSerializeOptions.h"
 
 #include "JSDOMConvertObject.h"
+#include "ErrorCode.h"
+#include "ZigGlobalObject.h"
 #include "JSDOMConvertSequences.h"
 #include <JavaScriptCore/JSArray.h>
 #include <JavaScriptCore/JSCInlines.h>
@@ -48,8 +50,15 @@ template<> StructuredSerializeOptions convertDictionary<StructuredSerializeOptio
         RETURN_IF_EXCEPTION(throwScope, {});
     }
     if (!transferValue.isUndefined()) {
+        // node: any failure to treat options.transfer as an iterable of objects
+        // surfaces as ERR_INVALID_ARG_TYPE rather than a generic sequence error.
+        auto transferScope = DECLARE_TOP_EXCEPTION_SCOPE(vm);
         result.transfer = convert<IDLSequence<IDLObject>>(lexicalGlobalObject, transferValue);
-        RETURN_IF_EXCEPTION(throwScope, {});
+        if (transferScope.exception()) {
+            (void)transferScope.tryClearException();
+            throwScope.throwException(&lexicalGlobalObject, createError(defaultGlobalObject(&lexicalGlobalObject), Bun::ErrorCode::ERR_INVALID_ARG_TYPE, "Optional options.transfer argument must be an iterable"_s));
+            return {};
+        }
     } else
         result.transfer = Converter<IDLSequence<IDLObject>>::ReturnType {};
     return result;
